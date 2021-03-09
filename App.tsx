2f06b38b93ea9ccc5df7b0b86c60cb8f4ef3c4b6
Ryan Peng <ryanpeng@Ryans-MacBook-Pro-3.local>
2021-03-08 17:11:33 -0700

    Added logos and updated landing page

--- App.tsx
+++ App.tsx
@@ -1,19 +1,129 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+
+export default class App extends Component<{}> {
+
+
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+
+        <TouchableOpacity style={styles.SpotifyStyle} activeOpacity={0.5}>
+
+          <Image
+            source={require('./assets/spotify_logo.png')}
+            style={styles.ImageIconStyle}
+          />
+
+          <View style={styles.SeparatorLine} />
+
+          <Text style={styles.TextStyle}> Login Using Spotify </Text>
+
+        </TouchableOpacity>
+
+
+        <TouchableOpacity style={styles.AppleMusicStyle} activeOpacity={0.5}>
+
+          <Image
+            source={require('./assets/applemusic_logo.png')}
+            style={styles.ImageIconStyle}
+          />
+
+          <View style={styles.SeparatorLine} />
+
+          <Text style={styles.TextStyle}> Login Using Apple Music </Text>
+
+        </TouchableOpacity>
+
+
+        <TouchableOpacity style={styles.SoundCloudStyle} activeOpacity={0.5}>
+
+          <Image
+            source={require('./assets/soundcloud_logo.png')}
+            style={styles.ImageIconStyle}
+          />
+
+          <View style={styles.SeparatorLine} />
+
+          <Text style={styles.TextStyle}> Login Using SoundCloud </Text>
+
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
   },
+
+  AppleMusicStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#69a6f9',
+    borderWidth: .5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+
+  },
+
+  SpotifyStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#1DB954',
+    borderWidth: .5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+
+  },
+
+  SoundCloudStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ff7700',
+    borderWidth: .5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+
+  },
+
+  ImageIconStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+
+  },
+
+  TextStyle: {
+
+    color: "#fff",
+    marginBottom: 4,
+    marginRight: 20,
+
+  },
+
+  SeparatorLine: {
+
+    backgroundColor: '#fff',
+    width: 1,
+    height: 40
+
+  }
+
 });
