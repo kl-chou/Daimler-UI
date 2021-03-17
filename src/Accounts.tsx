84ceaeb77ae784ee306dec56bdcc33b60982a890
Kinbert Chou <kl-chou@github.com>
2021-03-16 18:03:52 -0700

    moving login to Accounts.tsx, adding home

--- src/Accounts.tsx
+++ src/Accounts.tsx
@@ -1,22 +1,157 @@
-import React, { PureComponent } from 'react';
-import { StyleSheet, TouchableHighlight, View } from 'react-native';
-import { Button } from 'galio-framework';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Linking, Button } from 'react-native';
 
-class Accounts extends PureComponent {
+class Accounts extends Component {
+    handleClick = (AccountLink) => {
+        if (AccountLink == 'Spotify') Linking.openURL('https://accounts.spotify.com/en/login')
+        if (AccountLink == 'Apple') Linking.openURL('https://music.apple.com/login')
+        if (AccountLink == 'SoundCloud') Linking.openURL('https://soundcloud.com/djlogin')
+    };
 
     render() {
         return (
-            <View style={styles.container}>
+            <View style={styles.MainContainer}>
+                <View style={styles.MercedesContainer}>
+                    <Image
+                        source={require('../assets/mercedes_logo.png')}
+                        style={styles.MercedesLogo}
+                    />
 
+                    <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 72 }}> Mercedes-Benz </Text>
+                </View>
+
+                <View style={styles.ButtonContainer}>
+                    <TouchableOpacity onPress={() => { this.handleClick('Spotify') }} style={styles.SpotifyStyle} activeOpacity={0.5}>
+
+                        <Image
+                            source={require('../assets/spotify_logo.png')}
+                            style={styles.ImageIconStyle}
+                        />
+
+                        <View style={styles.SeparatorLine} />
+
+
+                        <Text style={styles.TextStyle}> Login Using Spotify </Text>
+
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity onPress={() => { this.handleClick('Apple') }} style={styles.AppleMusicStyle} activeOpacity={0.5}>
+
+                        <Image
+                            source={require('../assets/applemusic_logo.png')}
+                            style={styles.ImageIconStyle}
+                        />
+
+                        <View style={styles.SeparatorLine} />
+
+                        <Text style={styles.TextStyle}> Login Using Apple Music </Text>
+
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity onPress={() => { this.handleClick('SoundCloud') }} style={styles.SoundCloudStyle} activeOpacity={0.5}>
+
+                        <Image
+                            source={require('../assets/soundcloud_logo.png')}
+                            style={styles.ImageIconStyle}
+                        />
+
+                        <View style={styles.SeparatorLine} />
+
+                        <Text style={styles.TextStyle}> Login Using SoundCloud </Text>
+
+                    </TouchableOpacity>
+
+                </View>
             </View>
-        )
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+
+    MainContainer: {
         flex: 1,
-        justifyContent: "center",
-        paddingHorizontal: 10
-      },
-})
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: '#08112f'
+    },
+
+    AppleMusicStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#69a6f9',
+        borderWidth: .5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        margin: 5,
+
+    },
+
+    SpotifyStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#1DB954',
+        borderWidth: .5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        margin: 5,
+
+    },
+
+    SoundCloudStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#ff7700',
+        borderWidth: .5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        margin: 5,
+
+    },
+
+    ImageIconStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+    },
+
+    TextStyle: {
+        color: "#fff",
+        marginBottom: 4,
+        marginRight: 20,
+
+    },
+
+    SeparatorLine: {
+        backgroundColor: '#fff',
+        width: 1,
+        height: 40
+
+    },
+
+    MercedesLogo: {
+        width: 250,
+        height: 250,
+        resizeMode: 'contain'
+    },
+
+    MercedesContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+
+    ButtonContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    }
+});
+
+export default Accounts;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
-import Spotify from 'rn-spotify-sdk';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 class Home extends Component {
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <View style={styles.TopContainer}>
+                    <TouchableOpacity onPress={() => { }} style={styles.TouchableStyle} activeOpacity={0.5}>
 
+                        <Image
+                            source={require('../assets/spotify_logo.png')}
+                            style={styles.ImageIconStyle}
+                        />
+
+                        <Text style={styles.TextStyle}> Player </Text>
+
+                    </TouchableOpacity>
+                </View>
+            </View>
+        ); 
+    };
 }
+
+const styles = StyleSheet.create({
+    MainContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        backgroundColor: '#08112f'
+    },
+
+    TopContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 50
+    },
+
+    TouchableStyle: {
+        flexDirection: 'column'
+    },
+
+    ImageIconStyle: {
+        flex: 4,
+        height: '100%',
+        width: '100%'
+    },
+
+    TextStyle: {
+        flex: 1,
+        color: "#fff",
+        marginBottom: 4,
+        marginRight: 20,
+    }
+})
+
+export default Home;
